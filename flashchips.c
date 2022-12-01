00209d85f2d096347ecd959f61a13af93dccb5aa
Brian <brianbri6@hotmail.com>
2022-11-30 20:47:11 -0500

    Update flashchips.c

--- flashchips.c
+++ flashchips.c
@@ -10436,6 +10436,18 @@ const struct flashchip flashchips[] = {
 		.tested		= {.probe = OK, .read = OK, .erase = OK, .write = OK},
 		.probe		= PROBE_SPI_RDID,
 		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+		        {
+				.eraseblocks = { {4 * 128, 4096} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 128, 512} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 128, 256} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}
 		.printlock	= SPI_PRETTYPRINT_STATUS_REGISTER_BP3_SRWD, /* bit6 is quad enable */
 		.unlock		= SPI_DISABLE_BLOCKPROTECT_BP3_SRWD,
 		.write		= SPI_CHIP_WRITE256,
